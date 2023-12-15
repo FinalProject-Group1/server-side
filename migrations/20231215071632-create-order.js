11e3b57c91727a0586821eafe565570d66dd1c49
fadhildaffa <mdaffadhil98@gmail.com>
2023-12-15 16:18:43 +0700

    done setup until migrate

---
+++ migrations/20231215071632-create-order.js
@@ -0,0 +1,48 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Invoices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      SellerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id"
+        }
+      },
+      BuyerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id"
+        }
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+      orderStatus: {
+        type: Sequelize.STRING
+      },
+      paymentStatus: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Invoices');
+  }
+};
---
+++ models/sellerproduct.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class SellerProduct extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  SellerProduct.init({
+    ProductId: DataTypes.INTEGER,
+    SellerId: DataTypes.INTEGER,
+    stock: DataTypes.INTEGER,
+    price: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'SellerProduct',
+  });
+  return SellerProduct;
+};
