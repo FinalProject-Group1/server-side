68dac17bd6d72ad5673c2ddeda3dd46d068aeb0b
fauzandwip <fauzan27dp@gmail.com>
2023-12-19 13:13:24 +0700

    refactor: getProducts with query by category refactor: getAllSeller with eager loading Product feat: add getProductById route

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,19 +1,36 @@
-const { Product } = require('../models')
+const { Product } = require('../models');
 class ProductController {
+	static async getProducts(req, res, next) {
+		try {
+			const { category } = req.query;
+			const query = { where: {} };
 
-    static async getProducts(req, res, next){
-       try {
-        const products = await Product.findAll()
-        
-        res.status(200).json(products)
+			if (category) {
+				query.where.category = category;
+			}
 
-       } catch (error) {
-          next(error)
-       }
-    }
+			const products = await Product.findAll(query);
 
-    
+			res.status(200).json(products);
+		} catch (error) {
+			next(error);
+		}
+	}
 
+	static async getProductById(req, res, next) {
+		try {
+			const { id } = req.params;
+			const products = await Product.findOne({
+				where: {
+					id,
+				},
+			});
+
+			res.status(200).json(products);
+		} catch (error) {
+			next(error);
+		}
+	}
 }
 
-module.exports = ProductController
+module.exports = ProductController;
