4f652c055b7679cf80ff00b8b26ed8a563b0ca78
fauzandwip <fauzan27dp@gmail.com>
2023-12-20 22:35:48 +0700

    refactor: get buyer invoices

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,160 +2,208 @@ const { comparePassword } = require('../helpers/bcrypt');
 const { signToken, verifyToken } = require('../helpers/jwt');
 const { User, OrderItem } = require('../models');
 class UserController {
-  static async registerBuyer(req, res, next) {
-    try {
-      const { fullname, phoneNumber, address, password } = req.body;
-      if (!fullname || !phoneNumber || !address || !password) throw { name: 'FieldMissing' };
+	static async registerBuyer(req, res, next) {
+		try {
+			const { fullname, phoneNumber, address, password } = req.body;
+			if (!fullname || !phoneNumber || !address || !password)
+				throw { name: 'FieldMissing' };
 
-      const buyer = await User.create({ fullname, phoneNumber, address, password, role: 'buyer' });
+			const buyer = await User.create({
+				fullname,
+				phoneNumber,
+				address,
+				password,
+				role: 'buyer',
+			});
 
-      res.status(201).json(buyer);
-    } catch (error) {
-      next(error);
-    }
-  }
+			res.status(201).json(buyer);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-  static async login(req, res, next) {
-    try {
-      const { phoneNumber, password } = req.body;
-      // console.log(phoneNumber)
+	static async login(req, res, next) {
+		try {
+			const { phoneNumber, password } = req.body;
+			// console.log(phoneNumber)
 
-      if (!phoneNumber) throw { name: 'NullPhoneNumber' };
-      if (!password) throw { name: 'NullPassword' };
+			if (!phoneNumber) throw { name: 'NullPhoneNumber' };
+			if (!password) throw { name: 'NullPassword' };
 
-      const user = await User.findOne({ where: { phoneNumber } });
-      if (!user) throw { name: 'ErrorPhoneorPassword' };
+			const user = await User.findOne({ where: { phoneNumber } });
+			if (!user) throw { name: 'ErrorPhoneorPassword' };
 
-      const isValid = comparePassword(password, user.password);
-      if (!isValid) throw { name: 'ErrorPhoneorPassword' };
+			const isValid = comparePassword(password, user.password);
+			if (!isValid) throw { name: 'ErrorPhoneorPassword' };
 
-      let token = user.token;
+			let token = user.token;
 
-      if (!token) {
-        token = signToken({ id: user.id, phoneNumber: user.phoneNumber, role: user.role });
-        //update token di database
-        await user.update({ token });
-      }
+			if (!token) {
+				token = signToken({
+					id: user.id,
+					phoneNumber: user.phoneNumber,
+					role: user.role,
+				});
+				//update token di database
+				await user.update({ token });
+			}
 
-      const verifyTok = verifyToken(token);
-      const currentTime = Math.floor(Date.now() / 1000);
-      //verifyToken
-      if (verifyTok.exp && verifyTok.exp < currentTime) {
-        token = signToken({ id: user.id, phoneNumber: user.phoneNumber, role: user.role });
-        //update token di database
-        await user.update({ token });
-      }
+			const verifyTok = verifyToken(token);
+			const currentTime = Math.floor(Date.now() / 1000);
+			//verifyToken
+			if (verifyTok.exp && verifyTok.exp < currentTime) {
+				token = signToken({
+					id: user.id,
+					phoneNumber: user.phoneNumber,
+					role: user.role,
+				});
+				//update token di database
+				await user.update({ token });
+			}
 
-      res.status(200).json({ access_token: token });
-    } catch (error) {
-      next(error);
-    }
-  }
+			res.status(200).json({ access_token: token });
+		} catch (error) {
+			next(error);
+		}
+	}
 
-  static async getUserId(req, res, next) {
-    try {
-      const { id } = req.params;
+	static async getUserId(req, res, next) {
+		try {
+			const { id } = req.params;
 
-      const user = await User.findByPk(id, {
-        include: {
-          association: 'products',
-          include: {
-            association: 'product',
-          },
-        },
-      });
+			const user = await User.findByPk(id, {
+				include: {
+					association: 'products',
+					include: {
+						association: 'product',
+					},
+				},
+			});
 
-      // const user = await User.findByPk(id, {
-      //     include: {
-      //         association: 'products'
-      //     }
-      // })
+			// const user = await User.findByPk(id, {
+			//     include: {
+			//         association: 'products'
+			//     }
+			// })
 
-      res.status(200).json(user);
-    } catch (error) {
-      next(error);
-    }
-  }
-  static async profile(req, res, next) {
-    try {
-      const phoneNumber = req.user.phoneNumber;
-      const user = await User.findOne({ attributes: ['id', 'fullname', 'phoneNumber', 'address'], where: { phoneNumber } });
+			res.status(200).json(user);
+		} catch (error) {
+			next(error);
+		}
+	}
+	static async profile(req, res, next) {
+		try {
+			const phoneNumber = req.user.phoneNumber;
+			const user = await User.findOne({
+				attributes: [
+					'id',
+					'fullname',
+					'phoneNumber',
+					'address',
+					'city',
+					'role',
+					'token',
+					'shopName',
+					'saldo',
+				],
+				where: { phoneNumber },
+			});
 
-      res.status(200).json(user);
-    } catch (error) {
-      next(error);
-    }
-  }
+			res.status(200).json(user);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-  static async sellerInvoice(req, res, next) {
-    try {
-      const id = req.user.id;
-      const user = await User.findByPk(id, {
-        include: {
-          association: 'seller',
-          include: [{
-            model: OrderItem,
-            include: {
-              association: 'sellerproduct',
-              include: {
-                association: 'product',
-              },
-            },
-          },
-          {
-            association: 'buyer'
-          },],
-        },
-      });
+	static async sellerInvoice(req, res, next) {
+		try {
+			const id = req.user.id;
+			const user = await User.findByPk(id, {
+				include: {
+					association: 'seller',
+					include: [
+						{
+							model: OrderItem,
+							include: {
+								association: 'sellerproduct',
+								include: {
+									association: 'product',
+								},
+							},
+						},
+						{
+							association: 'buyer',
+						},
+					],
+				},
+			});
 
-      res.status(200).json(user);
-    } catch (error) {
-      next(error);
-    }
-  }
+			res.status(200).json(user);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-  static async buyerInvoice(req, res, next) {
-    try {
-      const id = req.user.id;
-      const user = await User.findByPk(id, {
-        include: {
-          association: 'buyer',
-          include: {
-            model: OrderItem,
-            include: {
-              association: 'sellerproduct',
-              include: {
-                association: 'product',
-              },
-            },
-          },
-        },
-      });
+	static async buyerInvoice(req, res, next) {
+		try {
+			const id = req.user.id;
+			const { orderStatus } = req.query;
+			// console.log(orderStatus, '>>> query');
 
-      res.status(200).json(user);
-    } catch (error) {
-      next(error);
-    }
-  }
+			const query = {
+				include: {
+					association: 'buyer',
+					include: [
+						{
+							model: OrderItem,
+							include: {
+								association: 'sellerproduct',
+								include: {
+									association: 'product',
+								},
+							},
+						},
+						{
+							association: 'seller',
+							attributes: {
+								exclude: ['password'],
+							},
+						},
+					],
+				},
+			};
 
-  static async mySellerProducts(req, res, next) {
-    try {
-      const id = req.user.id;
+			if (orderStatus) {
+				query.include.where = { orderStatus };
+			}
 
-      const user = await User.findByPk(id, {
-        include: {
-          association: 'products',
-          include: {
-            association: 'product',
-          },
-        },
-      });
+			// console.log(query);
+			const user = await User.findByPk(id, query);
 
-      res.status(200).json(user);
-    } catch (error) {
-      next(error);
-    }
-  }
+			res.status(200).json(user);
+		} catch (error) {
+			next(error);
+		}
+	}
+
+	static async mySellerProducts(req, res, next) {
+		try {
+			const id = req.user.id;
+
+			const user = await User.findByPk(id, {
+				include: {
+					association: 'products',
+					include: {
+						association: 'product',
+					},
+				},
+			});
+
+			res.status(200).json(user);
+		} catch (error) {
+			next(error);
+		}
+	}
 }
 
 module.exports = UserController;
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ const ProductController = require('../controllers/ProductController');
 const SellerProductController = require('../controllers/SellerProductController');
 const UserController = require('../controllers/UserController');
 const authentication = require('../middlewares/authentication');
-const { authorizationAdd, authorizationEdit, authorizationBuyer } = require('../middlewares/authorization');
+const {
+	authorizationAdd,
+	authorizationEdit,
+	authorizationBuyer,
+} = require('../middlewares/authorization');
 
 const error = require('../middlewares/errorHandler');
 
@@ -14,19 +18,27 @@ route.post('/login', UserController.login);
 route.post('/payment-success', PaymentGate.success);
 route.get('/products', ProductController.getProducts);
 route.get('/seller-products', SellerProductController.getAllSeller);
+route.get('/products/:id', ProductController.getProductById);
+route.get('/user/:id', UserController.getUserId);
 route.use(authentication);
 route.get('/user/seller-products', UserController.mySellerProducts);
 route.get('/invoice/:id', InvoiceController.getInvoice);
-route.get('/products/:id', ProductController.getProductById);
 route.get('/invoices-seller', UserController.sellerInvoice);
 route.get('/invoices-buyer', UserController.buyerInvoice);
 route.get('/profile', UserController.profile);
-route.get('/user/:id', UserController.getUserId);
-route.post('/seller-products', authorizationAdd, SellerProductController.sellerAdd);
+route.post(
+	'/seller-products',
+	authorizationAdd,
+	SellerProductController.sellerAdd
+);
 route.get('/seller-products/:id', SellerProductController.sellerFindById);
 route.post('/payment', authorizationBuyer, PaymentGate.getMidtransToken);
 route.post('/order', authorizationBuyer, InvoiceController.createInvoice);
-route.put('/seller-products/:id', authorizationEdit, SellerProductController.sellerEdit);
+route.put(
+	'/seller-products/:id',
+	authorizationEdit,
+	SellerProductController.sellerEdit
+);
 route.put('/seller-order', InvoiceController.editInvoiceSeller);
 route.put('/buyer-order', InvoiceController.editInvoiceBuyer);
 
